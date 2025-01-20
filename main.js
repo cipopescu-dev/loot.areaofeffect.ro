e5364fa3d0fd3be7dbf5663719f85b46537ae188
Ciprian Popescu <stefan-ciprian.popescu@ing.com>
2025-01-20 11:04:45 +0200

    Attendances

--- main.js
+++ main.js
@@ -7,10 +7,16 @@ const attendances = await readFirebase("/attendances");
 console.log(loot);
 console.log(attendances);
 Object.keys(loot)
-  .sort((playerName1, playerName2) => playerName1 > playerName2)
+  .sort(
+    (playerName1, playerName2) =>
+      attendances[playerName1] > attendances[playerName2]
+  )
   .forEach((playerName, idx) => {
     loot[playerName].sort((a, b) => a.resetNo < b.resetNo);
-    let row = createRow({ playerName, items: loot[playerName] });
+    let row = createRow({
+      playerName: `${playerName}(${attendances[playerName] || 0})`,
+      items: loot[playerName],
+    });
     row.forEach((tr) => tr.setAttribute("class", idx % 2 == 0 ? "tr2" : "tr1"));
     tBody.append(...row);
   });
