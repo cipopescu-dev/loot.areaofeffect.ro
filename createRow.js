3a2fadce3270d53561b54a82c1117a1fe875c433
Ciprian Popescu <stefan-ciprian.popescu@ing.com>
2025-01-24 03:06:14 +0200

    Some more stuff

--- createRow.js
+++ createRow.js
@@ -1,4 +1,4 @@
-export default function createRow({ playerName, items }) {
+export default function createRow({ playerName, items = [{}] }) {
   let nameTh = document.createElement("th");
   nameTh.setAttribute("rowspan", items.length);
   nameTh.setAttribute("scope", "rowgroup");
@@ -7,16 +7,19 @@ export default function createRow({ playerName, items }) {
   let tableRowElements = items.map(({ itemId, resetNo }) => {
     let itemTd = document.createElement("td");
     itemTd.setAttribute("scope", "row");
-
-    let itemLink = document.createElement("a");
-    itemLink.setAttribute(
-      "href",
-      `https://www.wowhead.com/classic/item=${itemId}`,
-    );
-    itemTd.append(itemLink);
+    if (itemId) {
+      let itemLink = document.createElement("a");
+      itemLink.setAttribute(
+        "href",
+        `https://www.wowhead.com/classic/item=${itemId}`
+      );
+      itemTd.append(itemLink);
+    } else {
+      itemTd.innerText = "-";
+    }
 
     let resetTd = document.createElement("td");
-    resetTd.innerText = resetNo;
+    resetTd.innerText = resetNo || "-";
     resetTd.style = "text-align:center";
 
     let row = document.createElement("tr");
